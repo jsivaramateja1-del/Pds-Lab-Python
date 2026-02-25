de9c77d41cbfc3a34e87a4345bd365b268c04a74
Siva Rama Teja <jsivaramateja1@gmail.com>
2026-02-25 18:16:55 +0530

    name changed in code changed from count to sum

--- Lab 1/Sum_Even_Odd.py
+++ Lab 1/Sum_Even_Odd.py
@@ -1,13 +1,13 @@
 # Problem 3: Sum of Even and Odd Numbers Separately
 N = int(input("Enter How many numbers you want to input :"))
 i = 0
-e_count = 0
-o_count = 0
+e_sum = 0
+o_sum = 0
 while i < N:
     num = int(input("Enter an number : "))
     if num % 2 == 0:
-        e_count += num
+        e_sum += num
     else:
-        o_count += num
+        o_sum += num
     i += 1
-print("Even numbers sum = ",e_count,",","Odd numbers sum = ",o_count)
+print("Even numbers sum = ",e_sum,",","Odd numbers sum = ",o_sum)
